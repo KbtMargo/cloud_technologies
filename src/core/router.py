e4a0a02fdf4a32b7cd81455770db21b347cbd507
KbtMargo <kubatiycom2@gmail.com>
2025-09-29 18:19:19 +0300

    swagger

--- src/core/router.py
+++ src/core/router.py
@@ -1,1 +1,14 @@
-# core/router.py
+from fastapi import APIRouter
+import datetime
+
+router = APIRouter(prefix="/common", tags=["common"])
+
+
+@router.get("/healthcheck")
+def healthcheck():
+    return {"status": "ok", "message": "Service is running"}
+
+
+@router.get("/time")
+def get_time():
+    return {"server_time": datetime.datetime.now().isoformat()}
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from src.core import router as common_routes
 
-app = FastAPI()
+app = FastAPI(
+    title="Lab3 FastAPI Project",
+    description="Lab project with FastAPI and Swagger UI",
+    version="0.1.0"
+)
 
-@app.get("/")
-def root():
-    return {"message": "Hello, FastAPI project!"}
-
+app.include_router(common_routes.router)
