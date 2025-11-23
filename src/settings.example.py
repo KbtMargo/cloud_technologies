f4e31cdc84e1c0116bc076717dd618b177959d2c
KbtMargo <kubatiycom2@gmail.com>
2025-11-23 16:15:52 +0200

    Remove secrets and add environment variables

---
+++ src/settings.example.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+class Settings(BaseSettings):
+    db_sync: str = "postgresql://username:password@localhost:5432/database"
+    db_async: str = "postgresql+asyncpg://username:password@localhost:5432/database"
+    
+    redis_url: str = "rediss://default:your_redis_password@your-redis-host:6379"
+    redis_TTL: int = 60
+    
+    azure_storage_connection_string: str = "DefaultEndpointsProtocol=https;AccountName=your_account;AccountKey=your_key;EndpointSuffix=core.windows.net"
+    azure_container_name: str = "your_container"
+    
+    model_config = ConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+settings = Settings()
